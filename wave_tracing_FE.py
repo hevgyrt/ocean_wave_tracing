added option for sides and fixed computation of wave ray direction

wave_tracing_FE.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 import logging
 import xarray as xa
 import pyproj
+import sys
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='wave_tracing.log', level=logging.INFO)
 logging.info('\nStarted')
+
+
 
 class Wave_tracing_FE():
     """ Class for tracing wave rays according to the geometrical optics
@@ -13,7 +16,7 @@ class Wave_tracing_FE():
     """
     def __init__(self, U, V,  nx, ny, nt, T, dx, dy, wave_period, theta0,
                  nb_wave_rays, domain_X0, domain_XN, domain_Y0, domain_YN,
-                 incoming_wave_direction,temporal_evolution=False, T0=None):
+                 incoming_wave_side,temporal_evolution=False, T0=None):
         """
         Args:
             U (float): eastward velocity 2D field
@@ -25,11 +28,14 @@ class Wave_tracing_FE():
             dx (int): Spatial resolution in x-direction. Units conforming to U
             dy (int): Spatial resolution in y-direction. Units conforming to V
             wave_period (float): Wave period.
-            theta0 (rad): Wave initial direction. In radians
-            nb_wave_rays (int): Number of wave rays to track
+            theta0 (rad): Wave initial direction. In radians.
+                         (0,.5*pi,pi,1.5*pi) correspond to going
+                         (right, up, left, down).
+            nb_wave_rays (int): Number of wave rays to track. NOTE: Should be
+                                equal or less to either nx or ny.
             domain_*0 (float): start value of domain area in X and Y direction
             domain_*N (float): end value of domain area in X and Y direction
-            incoming_wave_direction (str): side for incoming wave direction
+            incoming_wave_side (str): side for incoming wave direction
                                         [left, right, top, bottom]
 
             temporal_evolution (bool): flag if velocity field should change in time
@@ -44,13 +50,13 @@ class Wave_tracing_FE():
         self.theta0 = theta0
         self.nb_wave_rays = nb_wave_rays
 
-        self.domain_X0 = domain_X0
-        self.domain_XN = domain_XN
-        self.domain_Y0 = domain_Y0
-        self.domain_YN = domain_YN
+        self.domain_X0 = domain_X0 # left side
+        self.domain_XN = domain_XN # right side
+        self.domain_Y0 = domain_Y0 # bottom
+        self.domain_YN = domain_YN # top
+        self.i_w_side = incoming_wave_side
 
         self.temporal_evolution = temporal_evolution
-
 
         # Setting up X and Y domain
         self.x = np.linspace(domain_X0, domain_XN, nx)
@@ -65,7 +71,7 @@ class Wave_tracing_FE():
         self.theta = np.ma.zeros((nb_wave_rays,nt))
 
 
-        # decide velocity field if xarray object or
+        # make xarray data array of velocity field
         if not type(U) == xa.DataArray:
             self.U = xa.DataArray(U)
             self.V = xa.DataArray(V)
@@ -83,7 +89,7 @@ class Wave_tracing_FE():
         self.dt = T/nt
         self.nb_velocity_time_steps = len(self.U.time)
         if not temporal_evolution:
-            self.velocity_idt = np.zeros(nt,dtype=np.int)
+            self.velocity_idt = np.zeros(nt,dtype=int)
             logging.info('Vel idt : {}'.format(self.velocity_idt))
         else:
             t_velocity_field = U.time.data
@@ -123,6 +129,7 @@ class Wave_tracing_FE():
             instrinsic velocity (float): group or phase velocity depending on
                 flag
         """
+
         g=self.g
 
         if group_velocity:
@@ -150,23 +157,36 @@ class Wave_tracing_FE():
 
         kx0 = k0*np.cos(theta)
         ky0 = k0*np.sin(theta)
+        logger.info('wave: {}, {},{}'.format(k0,kx0,ky0))
         return k0,kx0,ky0
 
 
     def set_initial_condition(self):
-        """ Setting inital conditions before solving numertically.
+        """ Setting inital conditions before solving numerically.
         """
         k0, kx0, ky0 = self.wave(self.wave_period, self.theta0)
 
-        self.xr[:,0]=self.domain_X0
-        self.yr[:,0]=np.linspace(self.domain_Y0, self.domain_YN, self.nb_wave_rays)
+        if self.i_w_side == 'left':
+            self.xr[:,0]=self.domain_X0
+            self.yr[:,0]=np.linspace(self.domain_Y0, self.domain_YN, self.nb_wave_rays)
+        elif self.i_w_side == 'right':
+            self.xr[:,0]=self.domain_XN
+            self.yr[:,0]=np.linspace(self.domain_Y0, self.domain_YN, self.nb_wave_rays)
+        elif self.i_w_side == 'top':
+            self.xr[:,0]=np.linspace(self.domain_X0, self.domain_XN, self.nb_wave_rays)
+            self.yr[:,0]=self.domain_YN
+        elif self.i_w_side == 'bottom':
+            self.xr[:,0]=self.domain_Y0
+            self.yr[:,0]=np.linspace(self.domain_X0, self.domain_XN, self.nb_wave_rays)
+        else:
+            logger.error('Invalid initial wave direcion. Terminating.')
+            sys.exit()
+
         self.kx[:,0]=kx0
         self.ky[:,0]=ky0
         self.k[:,0]=k0
-        self.theta[:,0] = np.arctan(ky0/kx0) # self.theta0
-
-#    def fx_k(self, k,t):
-#        return arg1 + arg2
+        logger.info('theta: {}, {}'.format(theta0,np.arctan(ky0/kx0)))
+        self.theta[:,0] = self.theta0#np.arctan(ky0/kx0) # self.theta0
 
     def solve(self):
         """ Solve the geometrical optics equations numerically
@@ -213,55 +233,57 @@ class Wave_tracing_FE():
         counter=0
         for n in range(0,nt-1):
 
-            #theta[:,n+1] = theta[:,n] - dt*dudm
-            theta[:,n+1] = np.arctan(ky[:,n]/kx[:,n])
+            # find indices for each wave ray
+            idxs = np.array([self.find_nearest(x,xval) for xval in xr[:,n]])
+            idys = np.array([self.find_nearest(y,yval) for yval in yr[:,n]])
 
+            # compute the change in direction
+            if self.i_w_side == 'right' or self.i_w_side == 'left':
+                id_p1 = idys +1
+                id_p1[id_p1>=self.ny] = self.ny-1
+                dm = y[id_p1] - y[idys]
+                dudm = (U[velocity_idt[n],id_p1,idxs] - U[velocity_idt[n],idys,idxs])/dm
+                dvdm = (V[velocity_idt[n],id_p1,idxs] - V[velocity_idt[n],idys,idxs])/dm
+
+            elif self.i_w_side == 'top' or self.i_w_side == 'bottom':
+                id_p1 = idxs +1
+                id_p1[id_p1>=self.nx] = self.nx-1
+                dm = x[id_p1] - x[idxs]
+                dudm = (U[velocity_idt[n],idys,id_p1] - U[velocity_idt[n],idys,idxs])/dm
+                dvdm = (V[velocity_idt[n],idys,id_p1] - V[velocity_idt[n],idys,idxs])/dm
+
+            theta[:,n+1] = theta[:,n] - dt*(1/k[:,n])*(kx[:,n]*dudm + ky[:,n]*dvdm)#  dt*dudm
+            #theta[:,n+1] = np.arctan(ky[:,n]/kx[:,n])
+
+            # Compute group velocity
             cg_i = self.c_intrinsic(k[:,n],group_velocity=True)
 
             cg_i_x =  cg_i*np.cos(theta[:,n])
             cg_i_y =  cg_i*np.sin(theta[:,n])
 
-            idxs = np.array([self.find_nearest(x,xval) for xval in xr[:,n]])
-            idys = np.array([self.find_nearest(y,yval) for yval in yr[:,n]])
-
-#            K1_xr = dt*self.f(xr[:,n], t[k])
-#            K2_xr = dt*self.f(xr[:,n] + 0.5*K1, t[k] + dt2)
-#            K3_xr = dt*self.f(xr[:,n] + 0.5*K2, t[k] + dt2)
-#            K4_xr = dt*self.f(xr[:,n] + K3, t[k] + dt)
-#            u_new = u[k] + (1/6.0)*(K1 + 2*K2 + 2*K3 + K4)
-            #logging.info("xr:{}".format(xr[:,:]))
-            #logging.info("y:{}".format(x.shape))
-            #logging.info("yr:{}".format(yr))
-            #logging.info("idxs:{}".format(idxs.shape))
-            #logging.info("idxs:{}".format(idys.shape))
-            #logging.info("cg_x:{}".format(cg_i_x.shape))
-            #logging.info("U:{}, nbx: {}, nby: {}".format(U.shape,idxs.shape,idys.shape))
-            #logging.info("Uidx:{}".format(U[idys,idxs].shape))
-            #fig,ax = plt.subplots(figsize=(16,6))
-            #pc=ax.pcolormesh(x,y,U);plt.show()
-
-
-            #print(idxs)
-            #print(idys)
-            #print(xr.shape, U.shape, cg_i_x.shape)
-            #logger.info('idxs {}'.format(idxs))
-            #logger.info('idys {}'.format(idys))
+            # Advection
             xr[:,n+1] = xr[:,n] + dt*(cg_i_x+U[velocity_idt[n],idys,idxs])
             yr[:,n+1] = yr[:,n] + dt*(cg_i_y+V[velocity_idt[n],idys,idxs])
 
-
+            # Evolution in wave number
             kx[:,n+1] = kx[:,n] - dt*(kx[:,n]*dudx[velocity_idt[n],idys,idxs] + ky[:,n]*dvdx[velocity_idt[n],idys,idxs])
             ky[:,n+1] = ky[:,n] - dt*(kx[:,n]*dudy[velocity_idt[n],idys,idxs] + ky[:,n]*dvdy[velocity_idt[n],idys,idxs])
             k[:,n+1] = np.sqrt(kx[:,n+1]**2+ky[:,n+1]**2)
-            counter += 5
-            if counter==1:
-                logging.info(np.any(np.isnan(U[idys,idxs])))
-                logging.info(dt*n)
-                #logging.info([idd for idd in idxs])
-                #logging.info(idys)
-                #logging.info("idxs:{}".format(idys))
-                #logging.info("x:{}".format(x))
 
+            # Logging purposes
+            counter += 1
+            if counter==3 or counter==1000:
+                #logging.info(np.any(np.isnan(U[idys,idxs])))
+                #logging.info(dt*n)
+                #dm = np.sqrt(np.gradient(xr[:,n])**2 + np.gradient(yr[:,n])**2)
+                #logging.info('dm:{}'.format(len(dm)))
+                #logging.info('V: {}'.format(V[velocity_idt[n],idys,idxs]))
+                #logging.info(np.gradient(U[velocity_idt[n],idys,idxs],dm))
+                #logging.info([idd for idd in idxs])
+
+                logging.info("idxs:{}".format(cg_i_y))
+                #logging.info("x:{}".format(x))
+                #break
 
             """ FE
             xr[:,n+1] = xr[:,n] + dt*(cg_i_x+U[idys,idxs])
@@ -323,16 +345,20 @@ class Wave_tracing_FE():
 
         return xx,yy,hm
 
-    def to_latlon(self, proj4, flip_xy=False):
+    def to_latlon(self, proj4):
+        """ Method for reprojecting wave rays to latitude/longitude values
+
+        Args:
+            proj4 (str): proj4 string
+
+        Returns:
+            lons (2d): wave rays in longitude
+            lats (2d): wave rays in latitude
+        """
         lats = np.zeros((self.nb_wave_rays,self.nt))
         lons = np.zeros((self.nb_wave_rays,self.nt))
-        if not flip_xy:
-            for i in range(self.nb_wave_rays):
-                lons[i,:],lats[i,:] = pyproj.Transformer.from_proj(proj4,'epsg:4326', always_xy=True).transform(self.xr[i,:], self.yr[i,:])
-        else:
-            for i in range(self.nb_wave_rays):
-                lons[i,:],lats[i,:] = pyproj.Transformer.from_proj(proj4,'epsg:4326', always_xy=True).transform(self.yr[i,:], self.xr[i,:])
-
+        for i in range(self.nb_wave_rays):
+            lons[i,:],lats[i,:] = pyproj.Transformer.from_proj(proj4,'epsg:4326', always_xy=True).transform(self.xr[i,:], self.yr[i,:])
 
         return lons, lats
 
@@ -364,61 +390,60 @@ if __name__ == '__main__':
     U = u_eastwards.isel(time=1).to_array()[0].data
     V = v_northwards.isel(time=1).to_array()[0].data
 
+    # For testing
+    #U = np.zeros(U.shape)
+    #V = np.zeros(U.shape)
+
     X = u_eastwards.X
     Y = u_eastwards.Y
     nx = U.shape[1]
     ny = U.shape[0]
     nb_wave_rays = 120
-    #nx = 100
-    #ny=40
     dx=dy=800
 
-    T = 21000
+    T = 31000 #Total duration
     print("T={}h".format(T/3600))
-    nt = 2000
+    nt = 3000 # Nb time steps
     wave_period = 10
-    theta0 = 0#np.pi/8
+
+    i_w_side = 'left'#'top'
+    if i_w_side == 'left':
+        theta0 = 0 #Initial wave propagation direction
+    elif i_w_side == 'top':
+        theta0 = 1.4*np.pi#0#np.pi/8 #Initial wave propagation direction
+
     wt = Wave_tracing_FE(U, V, nx, ny, nt,T,dx,dy, wave_period, theta0, nb_wave_rays=nb_wave_rays,
                         domain_X0=X[0].data, domain_XN=X[-1].data,
-                        domain_Y0=Y[0].data, domain_YN=Y[-1].data)
+                        domain_Y0=Y[0].data, domain_YN=Y[-1].data,
+                        incoming_wave_side=i_w_side)
     wt.set_initial_condition()
     wt.solve()
+
+
+
+    ### PLOTTING ###
     fig,ax = plt.subplots(figsize=(16,6))
-    #pc=ax.pcolormesh(wt.x,wt.y,wt.U)
-    #pc=ax.pcolormesh(np.arange(len(u_eastwards.X))*800,np.arange(len(u_eastwards.Y))*800,wt.U)
-    pc=ax.pcolormesh(X,Y,wt.U.isel(time=0))
+
+    pc=ax.pcolormesh(X,Y,wt.U.isel(time=0),shading='auto')
+
+    ax.plot(wt.xr[:,0],wt.yr[:,0],'o')
+
     for i in range(wt.nb_wave_rays):
-        #for i in range(wt.ny):
-        #ax.plot(X[0].data+wt.xr[i,:],wt.yr[i,:],'-k')
         ax.plot(wt.xr[i,:],wt.yr[i,:],'-k')
     cb = fig.colorbar(pc)
                 #fig.savefig('T3')
-    plt.show()
 
-
-    proj4='+proj=stere +ellps=WGS84 +lat_0=90.0 +lat_ts=60.0 +x_0=3192800 +y_0=1784000 +lon_0=70'
+    # Georeference
+    proj4='+proj=stere +ellps=WGS84 +lat_0=90.0 +lat_ts=60.0 +x_0=3192800 +y_0=1784000 +lon_0=70' #NK800
     lons,lats=wt.to_latlon(proj4)
 
     import cartopy.crs as ccrs
     import cartopy.feature as cfeature
-    fig, ax = plt.subplots(frameon=False,figsize=(7,7),subplot_kw={'projection': ccrs.Mercator()})
+    fig2, ax2 = plt.subplots(frameon=False,figsize=(7,7),subplot_kw={'projection': ccrs.Mercator()})
 
     for i in range(wt.nb_wave_rays):
-        ax.plot(lons[i,:],lats[i,:],'-k',transform=ccrs.PlateCarree())
+        ax2.plot(lons[i,:],lats[i,:],'-k',transform=ccrs.PlateCarree())
 
-    # Deciding extent
-    lonmin, lonmax, latmin, latmax = 7,15.0,65,70
-    ax.set_extent([lonmin, lonmax, latmin, latmax]) #x0, x1, y0, y1) of the map in the given coordinate system.
-
-    # adding coastline
-
-    lscale = 'auto' # Scale for coasline
-    f = cfeature.GSHHSFeature(scale=lscale, levels=[1],
-            facecolor=cfeature.COLORS['land'])
-    ax.add_geometries(
-            f.intersecting_geometries([lonmin, lonmax, latmin, latmax]),
-            ccrs.PlateCarree(),
-            facecolor=cfeature.COLORS['land_alt1'],
-            edgecolor='black')
+    ax2.coastlines()
 
     plt.show()